c6f0a8431cce20a92dbdbea2790097eb91b14613
Elizabeth <elizabeth.dielentheis@gmail.com>
2016-04-09 11:56:48 -0400

    Added a second page?

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,6 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "i love it when you push my buttons";
 }
 
+function tryMe() {
+  document.getElementById("here").innerHTML = "did this work?";
+}
